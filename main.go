88c8b5fd8136e8448575833f0f410cb02fc42da7
byungsu.jeong <byungsu.jeong88@gmail.com>
2024-04-18 00:16:04 +0900

    Blockchain Structure2

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/byungsujeong/gocoin/blockchain"
+import (
+	"fmt"
+
+	"github.com/byungsujeong/gocoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }
