d118f7899ddd239d2b85b174fe1160b91c5df632
byungsu.jeong <byungsu.jeong88@gmail.com>
2024-04-18 23:46:55 +0900

    Refactoring into RESTful API2

--- main.go
+++ main.go
@@ -1,44 +1,7 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-)
-
-const port string = ":4000"
-
-type URLDescription struct {
-	URL         string `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         "/",
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         "/",
-			Method:      "GET",
-			Description: "Add A Block",
-			Payload:     "data:string",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-	// b, err := json.Marshal(data)
-	// utils.HandleErr(err)
-	// fmt.Fprintf(rw, "%s", b)
-}
+import "github.com/byungsujeong/gocoin/rest"
 
 func main() {
-	http.HandleFunc("/", documentation)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	rest.Start()
 }
