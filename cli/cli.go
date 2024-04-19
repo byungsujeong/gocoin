d020a7aad68972b9eaed7aaab92846c488e05a31
byungsu.jeong <byungsu.jeong88@gmail.com>
2024-04-19 16:18:16 +0900

    Set CLI

---
+++ cli/cli.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/byungsujeong/gocoin/explorer"
+	"github.com/byungsujeong/gocoin/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome\n\n")
+	fmt.Printf("Please user the following flags:\n\n")
+	fmt.Printf("-port:	Set the PORT of the server\n")
+	fmt.Printf("-mode:	Choose between 'SSR' and 'API'\n")
+	os.Exit(0)
+}
+
+func Start() {
+	if len(os.Args) == 1 {
+		usage()
+	}
+
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "API", "Choose between 'SSR' and 'API'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "API":
+		rest.Start(*port)
+	case "SSR":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+
+	fmt.Println(*port, *mode)
+}
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
-import (
-	"github.com/byungsujeong/gocoin/rest"
-)
+import "github.com/byungsujeong/gocoin/cli"
 
 func main() {
-	rest.Start(4000)
+	cli.Start()
 }
